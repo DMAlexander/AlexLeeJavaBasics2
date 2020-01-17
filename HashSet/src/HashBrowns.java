4fb651ea96b8072cc3859b2e8dd663a454fa5130
Devin Alexander <devinmalexander@gmail.com>
2020-01-17 08:13:37 -0600

    HashSet lesson completed

---
+++ HashSet/src/HashBrowns.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class HashBrowns {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> h = new HashSet<String>();
+		h.add("lemur");
+		h.add("orangatang");
+		h.add("spider monkey");
+		h.add("silverback gorilla");
+		String hello = "helloo";
+		System.out.println(h);
+
+	}
+
+}
